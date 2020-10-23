d655154aa7b7314289b18db77d3302da2d9ce057
ohwhatafeeling <pyro_michael@hotmail.com>
2020-10-23 13:46:31 +0930

    Show website after completing course

---
+++ suggest.php
@@ -0,0 +1,10 @@
+<?php
+$pageTitle = "Suggestion";
+$section = null;
+include('inc/header.php'); ?>
+
+  <div class="section page">
+    <h1><?php echo "<a href='catalog.php'>Full Catalog</a> &gt; " . $pageTitle; ?></h1>
+  </div>
+
+<?php include('inc/footer.php'); ?>
